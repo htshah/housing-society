ff035a8c2000cfcbff63c6eb077e90b13e65c7c1
Het Shah <htshah60@gmail.com>
2019-06-20 19:21:56 +0530

    feat: Added user_id to event table

--- database/factories/EventFactory.php
+++ database/factories/EventFactory.php
@@ -6,10 +6,12 @@ use App\Event;
 use Faker\Generator as Faker;
 
 $factory->define(Event::class, function (Faker $faker) {
+    $users = App\User::pluck('id')->toArray();
     return [
         'title' => $faker->words(5, true),
         'description' => $faker->sentence(3, true),
         'amount' => rand(100, 300),
         'end_time' => (new \DateTime("+30 day"))->format('Y-m-d'),
+        'user_id' => $faker->randomElement($users),
     ];
 });
--- database/migrations/2019_06_20_110226_create_event_table.php
+++ database/migrations/2019_06_20_110226_create_event_table.php
@@ -15,11 +15,14 @@ class CreateEventTable extends Migration
     {
         Schema::create('event', function (Blueprint $table) {
             $table->bigIncrements('id');
+            $table->unsignedBigInteger('user_id');
             $table->string("title");
             $table->string("description");
             $table->dateTime("end_time");
             $table->float('amount', 8, 2);
             $table->timestamps();
+
+            $table->foreign('user_id')->references('id')->on('user');
         });
     }
 

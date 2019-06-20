9620006149d857c273e173b17d235d0d572a9386
Het Shah <htshah60@gmail.com>
2019-06-20 18:43:52 +0530

    feat: Added basic auth routes

--- routes/api.php
+++ routes/api.php
@@ -11,8 +11,14 @@ use Illuminate\Http\Request;
 | routes are loaded by the RouteServiceProvider within a group which
 | is assigned the "api" middleware group. Enjoy building your API!
 |
-*/
+ */
 
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
+});
+
+Route::prefix('auth')->group(function () {
+    Route::post('login', 'UserController@login');
+});
+Route::group(['middleware' => ['jwt-auth', 'api-header']], function () {
 });
